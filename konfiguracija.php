3089e6f06b67c6d8ad4d21ee79a39dd9156665c8
Antun Matanović <antun.matanovic@gmail.com>
2015-10-27 19:58:04 +0100

    konfiguracija

---
+++ konfiguracija.php
@@ -0,0 +1,8 @@
+<?php
+$server="localhost";
+$baza="omsdm";
+$korisnik="root";
+$lozinka="admin";
+$putanja="/InfiniteLoop/";
+$veza=new PDO("mysql:host=" . $server . ";dbname=" . $baza,$korisnik,$lozinka);
+$veza->exec("set names utf8;");
---
+++ script.sql
@@ -0,0 +1,3 @@
+drop database if exists omsdm;
+create database omsdm character set utf8 collate utf8_general_ci;
+use omsdm;
