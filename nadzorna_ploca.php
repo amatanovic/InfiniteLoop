a0cb62853ae5f97c1a5920c639a5a065b2db39f7
Antun Matanović <antun.matanovic@gmail.com>
2015-10-31 02:04:42 +0100

    komit

--- nadzorna_ploca.php
+++ nadzorna_ploca.php
@@ -70,11 +70,15 @@ if(!isset($_SESSION['autoriziran'])){
 $izraz=$veza->prepare("select * from korisnik");
 $izraz->execute();
 $korisnici=$izraz->fetchALL(PDO::FETCH_OBJ);
-foreach ($korisnici as $korisnik): ?>
+foreach ($korisnici as $korisnik): 
+if ($korisnik->admin != 1):
+?>
 <p>
 <img src="<?php echo $korisnik->avatar; ?>" style="width:20%" />
 </p>
-<?php endforeach; ?>
+<?php 
+endif;
+endforeach; ?>
 </div>
       </div>
     </div>
