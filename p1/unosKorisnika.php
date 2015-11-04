0a073e946a4596af5c71f1820251bb67254e8b2b
Antun Matanović <antun.matanovic@gmail.com>
2015-11-04 11:38:48 +0100

    novi projekt

--- p1/unosKorisnika.php
+++ p1/unosKorisnika.php
@@ -1,27 +1,31 @@
 <p>
-Unos novog korisnika
+<h4>Unos novog korisnika</h4>
  <form method="POST" action="" enctype="multipart/form-data">
     <input class="odaberi-avatar" style="margin-top:1em;width:72%;margin-left: 16%;border: none;" type="file" name="avatar" id="avatar" accept="image/*" />
-      <div class="input-group">
-      <span class="input-group-addon">Ime</span>
+      <div class="form-group input-group">
+      <div class="input-group privatni-profil">
+      <span class="input-group-addon profil">Ime</span>
       <input type="text" name="ime" class="form-control" aria-describedby="basic-addon1" />
     </div>
-    <div class="input-group">
-      <span class="input-group-addon">Prezime</span>
+    <div class="input-group privatni-profil">
+      <span class="input-group-addon profil">Prezime</span>
       <input type="text" name="prezime" class="form-control" aria-describedby="basic-addon1" />
     </div>
-     <div class="input-group">
-      <span class="input-group-addon">Korisničko ime</span>
+     <div class="input-group privatni-profil">
+      <span class="input-group-addon profil">Korisničko ime</span>
       <input type="text" name="korisnicko_ime" class="form-control" aria-describedby="basic-addon1" />
     </div>
-    <div class="input-group">
-      <span class="input-group-addon">Lozinka</span>
+    <div class="input-group privatni-profil">
+      <span class="input-group-addon profil">Lozinka</span>
       <input type="password" name="lozinka" class="form-control" aria-describedby="basic-addon1" />
     </div>
-    <div class="input-group">
-      <span class="input-group-addon">Ponovi lozinku</span>
+    <div class="input-group privatni-profil">
+      <span class="input-group-addon profil">Ponovi lozinku</span>
       <input type="password" name="lozinka2" class="form-control" aria-describedby="basic-addon1" />
     </div>
-    <input type="submit" value="Unesi novog korisnika" name="registracija" /><br />
+    <div class="row profilBtn">
+    <input type="submit" value="Unesi novog korisnika" name="registracija" class="btn btn-default" />
+    </div>
+    </div>
   </form>
 </p>
