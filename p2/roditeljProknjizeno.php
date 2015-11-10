1aef6314c50cde8e1d18c310e4b8f319d0cdd439
amihaljevic <andrea.mihaljevic@hotmail.com>
2015-11-10 19:31:42 +0100

    proknjizeno 1.0

---
+++ p2/roditeljProknjizeno.php
@@ -0,0 +1,25 @@
+<?php
+include "head.php"; ?>
+
+<body class="bodyRavnatelj">
+  <div class="container-fluid">
+  	<div class="row">
+  		<div class="col-md-12">
+  <header class="headerRavnatelj">
+  <h2>Dobrodošli</h2>      
+  <h2 class="imeRoditelja">Mama Vilček</h2>
+  <input type="submit" name="registracija" value="Odjavi se" class="btn btn-default pull-right ravnateljBtn" />           
+  </header>
+</div>
+</div>
+<div class="row profesorUplate">
+	<div class="col-md-12">
+  <div class="thumbnail roditelj">
+    <img src="slike/uplatnica.gif" alt="Uplatnica">
+  </div>
+  <p>Poštovani, Vaša uplata je uspješno proknjižena.</p>
+  <p>Poštovani, Vaša uplata još nije proknjižena. Molimo Vas pričekajte obavijest o proknjiženju.</p>
+</div>
+</div>
+<?php include "footer.php"; ?>
+</body>
