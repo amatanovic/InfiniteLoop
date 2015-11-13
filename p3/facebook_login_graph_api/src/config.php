884c8612bb515594649497b89ba73e12e0ab4120
Antun Matanović <antun.matanovic@gmail.com>
2015-11-13 19:57:42 +0100

    komit

--- p3/facebook_login_graph_api/src/config.php
+++ p3/facebook_login_graph_api/src/config.php
@@ -1,5 +1,5 @@
 <?php
-$config['callback_url']         =   'http://project.comuv.com/p3/facebook_login_graph_api/index.php?fbTrue=true'; //   /?fbTrue=true allow you to code process section.
+$config['callback_url']         =   'http://localhost/p3/facebook_login_graph_api/index.php?fbTrue=true'; //   /?fbTrue=true allow you to code process section.
 
 //Facebook configuration
 $config['App_ID']      =   '1086359581374842';
