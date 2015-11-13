dd0e93ab314fb2b86a18c1b924a99ff499f9e313
tvilcek <vilcek.tena@gmail.com>
2015-11-13 23:14:59 +0100

    prošla skripta

--- p3/script.sql
+++ p3/script.sql
@@ -37,6 +37,7 @@ naziv varchar(250)
 drop table if exists slike;
 create table slike (
 sifra int not null primary key auto_increment,
+putanja varchar(250),
 korisnik int,
 kategorija int,
 djelatnik int,
@@ -76,7 +77,7 @@ naziv varchar(250)
 
 alter table frizerski_salon add foreign key (mjesto) references mjesto(sifra);
 alter table korisnik add foreign key (mjesto) references mjesto(sifra);
-alter table korisnik add foreign key (salon) references salon(sifra);
+alter table korisnik add foreign key (salon) references frizerski_salon(sifra);
 alter table check_in add foreign key (korisnik) references korisnik(sifra);
 alter table slike add foreign key (korisnik) references korisnik(sifra);
 alter table slike add foreign key (djelatnik) references korisnik(sifra);
@@ -93,7 +94,19 @@ insert into frizerski_salon (adresa, mjesto, kontakt, facebook, naziv, kor_ime, 
 insert into korisnik (ime, prezime, salon, avatar) values ("Antun", "Matanović", 1, "slike/avatar_1.jpg");
 insert into korisnik (ime, prezime, mjesto, kor_ime, lozinka) values ("Andrea", "Mihaljević", 1, "amihaljevic@gmail.com", md5("123"));
 
-insert into usluga (naziv, trajanje) values ("", "");	
+insert into usluga (naziv, trajanje) values ("šišanje kratke kose", "00:20:00"), ("šišanje duge kose","00:30:00"), ("frizure (kratke kose)","00:30:00"),
+("frizure (duge kose)","00:45:00"), ("bojanje kratke kose","01:00:00"), ("bojanje duge kose","01:30:00"), 
+("pranje i sušenje kose kratke kose","00:15:00"), ("pranje i sušenje duge kose","00:30:00"), ("pramenovi (kratka kosa)","01:00:00"),
+("pramenovi (duga kosa)","02:00:00"), ("trajna (kratka kosa)","02:00:00"), ("trajna (duga kosa)","02:30:00"), ("korekcija obrva","00:15:00"),
+("muško brijanje","00:20:00");
+
+insert into kategorija (naziv) values ("Duga kosa"), ("Srednje duga kosa"), ("Kratka kosa");
+
+insert into rezervacija (korisnik, salon, usluga, vrijeme_pocetka, vrijeme_zavrsetka) values (2, 1, 4, "2015-11-14 14:00:00", "2015-11-14 14:45:00");
+
+insert into check_in (vrijeme, korisnik) values ("2015-11-14 08:00:00", 1);
+
+insert into slike (putanja, korisnik, djelatnik, kategorija, vrijeme) values ("slike/andrea.jpg", 2, 1, 3, "2015-02-15 09:45:00");	
 
 
 
