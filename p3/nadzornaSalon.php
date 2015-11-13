a492d81be1ebdf9b598a490bd90139e642aa5100
amihaljevic <andrea.mihaljevic@hotmail.com>
2015-11-13 21:33:01 +0100

    adf

---
+++ p3/nadzornaSalon.php
@@ -0,0 +1,52 @@
+<?php include "head.php"; ?>
+<body class="indexNadzorna">
+	<div class="container-fluid">
+		<header>
+			<div class="navbar navbar-default">
+    			<div class="navbar-collapse collapse navbar-responsive-collapse">
+    				<h3>Frizerski salon Mata<span class="mdi-hardware-keyboard-arrow-down" style="color: #fff"></h3>
+        			<ul class="nav navbar-nav navbar-right">
+            			<li><a href="#">|	Arhiva Slika	|</a></li>
+            			<li><a href="#">	Postavke	|</a></li>
+            			<li><a href="#">	Odjava	|</a></li>
+        			</ul>
+    			</div>
+ 			</div>
+		</header>
+		<div class="row">
+			<div class="col-md-8 divRaspored">
+				<table class="table table-striped table-hover ">
+				    <thead>
+				        <tr>
+				            <th>IME I PREZIME</th>
+				            <th>USLUGA</th>
+				            <th>TERMIN</th>
+				            <th>SLIKA</th>
+				        </tr>
+				    </thead>
+				    <tbody>
+				        <tr>
+				            <td>Manuela Mikulecki</td>
+				            <td>šišanje duge kose</td>
+				            <td>12.11.2015. <br> 09:00-09:30</td>
+				            <td><span class="mdi-content-add-circle-outline" style="color: #c21e5c"></span></td>
+				        </tr>
+				    </tbody>
+				</table>
+			</div>
+			<div class="col-md-4 divInformacije">
+				<h2>Frizerski salon Mata</h2>
+				<address><span class="mdi-maps-place" style="color: black"></span>Gundulićeva 43b, Osijek</address>
+				<p><span class="mdi-maps-local-phone" style="color: black"></span>091/375-487</p>
+				<p><img src="facebook.png">http://facebook.com</p>
+				<h2>Usluge</h2>
+				<ol>
+					<li>šišanje kratke kose</li>
+					<li>šišanje duge kose</li>
+					<li>frizure (kratka kosa)</li>
+					<li>frizure (duga kosa)</li>
+				</ol>
+			</div>
+		</div>
+	</div>
+</body>
