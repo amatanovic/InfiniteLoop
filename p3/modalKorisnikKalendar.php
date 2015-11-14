c94b1ed4fb47a4a4a61ef256b209ecd50041d941
amihaljevic <andrea.mihaljevic@hotmail.com>
2015-11-14 05:22:14 +0100

    gfd

--- p3/modalKorisnikKalendar.php
+++ p3/modalKorisnikKalendar.php
@@ -4,10 +4,41 @@
                 <div class="modal-content">
                     <div class="modal-header">
                         <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-                        <h4 class="modal-title">Modal title</h4>
                     </div>
                     <div class="modal-body">
-                        <p>One fine body…</p>
+                        <p>Odaberite uslugu i termin koji Vam odgovara kako bi Vam se ponudila mogućnost odabira frizerskog salona.</p>
+                        <div class="form-group">
+                            <label for="select" class="col-lg-2 control-label">Selects</label>
+                            <div class="col-md-10">
+                                <select class="form-control" id="select">
+                                    <option>1</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-5">
+                                <div class="checkbox">
+                                    <label>
+                                        <input type="checkbox">
+                                        08:00 - 09:30
+                                        <span class="checkbox-material">
+                                            <span class="check checkModal"></span>
+                                        </span> 
+                                    </label>
+                                </div>
+                            </div>
+                            <div class="col-md-5">
+                                <div class="checkbox">
+                                    <label>
+                                        <input type="checkbox">
+                                        Frizerski prvi
+                                        <span class="checkbox-material">
+                                            <span class="check checkModal"></span>
+                                        </span> 
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                     <div class="modal-footer">
                         <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
---
+++ p3/modalSlike.php
@@ -0,0 +1,57 @@
+<div class="modal" id="modalKorisnikKalendar">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+                        <h3 class="modal-title">Kategorija</h3>
+                    </div>
+                    <div class="modal-body">
+                        <div class="checkbox">
+                            <label>
+                                <input type="checkbox">
+                                Duga kosa
+                                <span class="checkbox-material">
+                                    <span class="check checkModal"></span>
+                                </span> 
+                            </label>
+                        </div>
+                        <div class="checkbox">
+                            <label>
+                                <input type="checkbox">
+                                Srednje duga kosa
+                                <span class="checkbox-material">
+                                    <span class="check checkModal"></span>
+                                </span> 
+                            </label>
+                        </div>
+                        <div class="checkbox">
+                            <label>
+                                <input type="checkbox">
+                                Kratka kosa
+                                    <span class="checkbox-material">
+                                        <span class="check checkModal"></span>
+                                    </span> 
+                            </label>
+                        </div>
+                        <div class="form-group">
+                            <label for="inputFile" class="col-lg-2 control-label">File</label>
+                            <div class="col-md-10">
+                                <input type="text" readonly="" class="form-control floating-label" placeholder="Browse...">
+                                <input type="file" id="inputFile" multiple="">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="inputFile" class="col-lg-2 control-label">File</label>
+                            <div class="col-md-10">
+                                <input type="text" readonly="" class="form-control floating-label" placeholder="Browse...">
+                                <input type="file" id="inputFile" multiple="">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary">Save changes</button>
+                    </div>
+                </div>
+            </div>
+        </div>
