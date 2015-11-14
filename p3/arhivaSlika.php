8ee938e3866cc6b97c026dcadeaf498adcb3b672
amihaljevic <andrea.mihaljevic@hotmail.com>
2015-11-14 02:45:34 +0100

    arhiva

---
+++ p3/arhivaSlika.php
@@ -0,0 +1,81 @@
+<?php include "head.php"; ?>
+<body class="indexNadzorna">
+	<div class="container-fluid">
+		<header>
+			<div class="navbar navbar-default">
+    			<div class="navbar-collapse collapse navbar-responsive-collapse">
+    				<h3>Frizerski salon Mata<span class="mdi-hardware-keyboard-arrow-down" style="color: #fff"></h3>
+        			<ul class="nav navbar-nav navbar-right">
+            			<li><a href="#">|	Arhiva Slika	|</a></li>
+            			<li><a href="#">	Postavke	|</a></li>
+            			<li><a href="#">	Odjava	|</a></li>
+        			</ul>
+    			</div>
+ 			</div>
+ 			<div class="row">
+ 				<p style="color: #fff; padding-left: 50px; padding-right: 15px; margin-right: 15px;">Tena Vilček</p>
+ 				
+ 			</div>
+		</header>
+		<div class="row">
+			<div class="col-md-8 divPostavke">		
+				<div class="row">
+				  	<div class="col-md-6">
+				    	<div class="thumbnail">
+				      		<img src="slike/andrea.jpg" alt="...">
+				    	</div>
+				    	<div class="caption">
+				    	<p style="text-align: center">Andrea Mihaljević<span class="mdi-social-share"></span></p>
+				    	</div>
+				  	</div>
+				</div>
+			</div>
+			<div class="col-md-4 divInformacije" style="height: 100%">
+				<h2>Kategorija</h2>
+				<div class="checkbox">
+				    <label>
+				        <input type="checkbox">
+				        <span class="checkbox-material">
+				            <span class="check checkArhiva"></span>
+				        </span> Duga kosa
+				    </label>
+				    </div>
+				    <div class="checkbox">
+				        <label>
+				            <input type="checkbox">
+				            <span class="checkbox-material">
+				               	<span class="check checkArhiva"></span>
+				            </span> Srednje duga kosa
+				        </label>
+				    </div>
+				    <div class="checkbox">
+				        <label>
+				            <input type="checkbox">
+				            <span class="checkbox-material">
+			                	<span class="check checkArhiva"></span>
+			                </span> Kratka kosa
+			            </label>
+			        </div>
+			        <h2>Mjesec</h2>
+			        <h2>Djelatnik</h2>
+			        <div class="checkbox">
+				        <label>
+				            <input type="checkbox">
+				            <span class="checkbox-material">
+				               	<span class="check checkArhiva"></span>
+				            </span> Tena Vilček
+				        </label>
+				    </div>
+				    <div class="checkbox">
+				        <label>
+				            <input type="checkbox">
+				            <span class="checkbox-material">
+			                	<span class="check checkArhiva"></span>
+			                </span> Manuela Mikulceki
+			            </label>
+			        </div>
+			    </div>
+			</div>
+		</div>
+	</div>
+</body>
